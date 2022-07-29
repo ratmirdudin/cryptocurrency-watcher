13647a24d608960a8d95d3264725d9123be688a0
Ratmir <ratmirdudin@mail.ru>
2022-07-29 11:32:40 +0600

    init commit

---
+++ src/main/java/com/ratmirdudin/cryptocurrency_watcher/CryptocurrencyWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.ratmirdudin.cryptocurrency_watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratmirdudin/cryptocurrency_watcher/CryptocurrencyWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratmirdudin.cryptocurrency_watcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
